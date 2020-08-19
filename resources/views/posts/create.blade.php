9b757d3866c3e510a87cbedd421f63e6aca4a065
Enrique J. Pizarro <enriquejavierpizarro@gmail.com>
2020-08-19 14:40:32 -0400

    added old function to create view to keep the data on input boxes when form reloads after submit when validation fails

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,12 +5,14 @@
         @csrf{{-- This is a token to prevent exploits on the form, without it renders an error --}}
      <p>   
         <label>Title</label>
-        <input type="text" name="title"/>
+        <input type="text" name="title" value="{{old('title')}}"/>{{-- value="old()" if the data does not pass
+        the validation, when the form reloads the data is kept in the input boxes to
+        faccilitate the correction of it by use  --}}
      </p>
 
      <p>   
         <label>Content</label>
-        <input type="text" name="content"/>
+        <input type="text" name="content" value="{{old('content')}}"/>
      </p>
 
      @if($errors->any()){{-- Check notebook notes, $errors is a session variable(available to all views)
