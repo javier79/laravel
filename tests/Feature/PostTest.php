261fcbe4ffc733246801ee75176340ded332c97c
Enrique J. Pizarro <enriquejavierpizarro@gmail.com>
2020-08-30 11:53:48 -0400

    Added more test on PostTest named testStoreValid and commented it

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -38,6 +38,28 @@ class PostTest extends TestCase
 
 
     }
+
+    public function testStoreValid()
+    {
+        //Arrange
+        /*$params refers to parameters. Below associative array wants to specify the scenario where 
+        key 'title' and 'content' references a string that comply with the validation for both text fields.
+        values 'Valid title' and 'At least 10 characters' are set arbitraryly to model string lenght */
+        $params=[
+            'title'=> 'Valid title',
+            'content'=>'At least 10 characters'
+        ];
+        /*Here we are simulating an http request in the browser as if a form have been submitted,
+        with the paramters requiring and modeling the state of the submitted data(validated/correct)*/
+        //Act
+        $this->post('/posts', $params)/*Redirects to the /post associated to $params values.post() verb
+        is used as we are simulating a form request, also needed to access assertStatus()/assertSessionHas()*/
+        //Assert
+        ->assertStatus(302)//Checking for successful redirect(302 is http code for successful redirect)
+        ->assertSessionHas('status');/*test key name 'status' is present in the session found at
+        PostController($request->session()->flash('status', 'Blog post was updated!')*/
+        $this->assertEquals(session('status'), 'Blog post was created!');/*test if added/flash message text display as intended, session('status') reads 'status' value and compares to second parameter */ 
+    }
 }
 
 //Reminder:In each test we are running all migrations
