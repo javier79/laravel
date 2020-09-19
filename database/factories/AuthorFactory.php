89f7f8b9a5a5a531e302ef9d6912719fd5d38bc8
Enrique J. Pizarro <enriquejavierpizarro@gmail.com>
2020-09-19 19:38:21 -0400

    comments

--- database/factories/AuthorFactory.php
+++ database/factories/AuthorFactory.php
@@ -11,8 +11,14 @@ $factory->define(Author::class, function (Faker $faker) {
     ];
 });
 /*Calls for the previously created Author instance and pass it to closure function(callback)
-as $author and previously created instance of Faker as $faker*/
+as $author and previously created instance of Faker as $faker. afterCreating means
+that the callback is executing after Author object was instantiated and saved*/
 $factory->afterCreating(App\Author::class, function ($author, $faker)
+{
+    $author->profile()->save(factory(App\Profile::class)->make());//access relation, instantiate object and save, create(make()).
+});
+//Another callback can be executed after the object have been instantiated(afterMaking()) but not saved yet
+$factory->afterMaking(App\Author::class, function ($author, $faker)
 {
     $author->profile()->save(factory(App\Profile::class)->make());//access relation, instantiate object and save, create(make()).
 });
