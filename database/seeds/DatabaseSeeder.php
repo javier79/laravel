639ca8b2338aae40ee2471ab89e9133debc74f75
Enrique J. Pizarro <enriquejavierpizarro@gmail.com>
2020-10-14 23:46:05 -0400

    edited DatabbaseSeeder, UserFactory and BlogPostFactory(copy and pasted a function structure meaning it weas modified check notebook II and comments)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        /*DB::table('users')->insert([
             //from UserFactory.php
             'name' => 'John Doe',
             'email' => 'john@laravel.test',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-        ]);
+        ]);AS AT THE TIME ONLY WANTED TO OVERWRITE NAME AND EMAIL 
+        WE WILL USE THE LINE BELOW INSTEAD, TO SEE THE WHOLE BLOCK OF CODE
+        PRESS ON THE THE DROP DOWN ARROW AT THE LEFT IN THE BEGINNING 
+        OF THE BLOCK*/
+
+        factory(App\User::class)->states('john-doe')->create();
+
+        factory(App\User::class, 20)->create();/*generates(UserFactory.php) and add 20 records, we keep
+        the hard coded record we create in the run()*/
     }
+
 }
