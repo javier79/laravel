dae27af00111b27318f0f24c44ab064309409042
Enrique J. Pizarro <enriquejavierpizarro@gmail.com>
2020-10-16 16:47:34 -0400

    Edits take place in BlogPost.php check notebookII and comments

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -15,4 +15,17 @@ class BlogPost extends Model
     {
         return $this->hasMany('App\Comment');
     }
+
+    public static function boot()
+    {
+        parent::boot();//reference to Model class where boot() lives
+
+        static::deleting(function(BlogPost $blogPost){
+            /*dd('I was deleted');/*With this statement we are testing that the callback for deleting()
+            is being executed before the deletion when this execute the deletion
+            does not occur as it is stopped before it execute*/
+
+            $blogPost->comments()->delete();//deletes all the comments for each blogpost that we delete      
+        });
+    }
 }
