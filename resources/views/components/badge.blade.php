b7ad7b74cbc158605e8eda7c78167e0c3fdf8489
Enrique J. Pizarro <enriquejavierpizarro@gmail.com>
2020-11-22 17:04:14 -0400

    editions take place on badge.blade.php and show.blade.php check notebookII comments and questions on tutorials for component aliases lesson on udemy

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,7 +1,10 @@
-<span class="badge badge-{{ $type ?? 'success' }}">{{-- if a type(another slot variable check notebookII) 
-     is specified use the bootstrap class that references $type variable, otherwise
-     use default 'success'(bootstrap class) that references the green badge--}}
-    {{ $slot }}{{-- references the text to be displyed and that resides on show.blade
-        this variable is native to laravel and used like this, there are other 'slot' variables like $type for other 
-        uses. --}}
-    </span> 
+    @if(!isset($show) || $show)
+        <span class="badge badge-{{ $type ?? 'success' }}">{{-- if a type(another slot variable check notebookII) 
+        is specified use the bootstrap class that references $type variable, otherwise
+        use default 'success'(bootstrap class) that references the green badge--}}
+        {{ $slot }}{{-- references the text to be displyed and that resides on show.blade
+            this variable is native to laravel and used like this, there are other 'slot' variables like $type for other 
+            uses. --}}
+        </span> {{-- we change the tag from <div> to <span> due div sometimes force the use of another an extra paragraph 
+            so we change to span to make it more usable sometimes like in this case there is not a great change on display --}}
+    @endif
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,7 +7,18 @@
     {{--The links for this view is http://laravel.test/posts/1 or http://laravel.test/posts/2  
     (/1 and /2 refers the primary key which is ID), as i understand it's determined by the URI in the route:list--}}
         
-        <h1>{{ $post-> title }}</h1>
+        <h1>
+            {{ $post-> title }}
+
+            {{--@if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 30){{-- We eliminate the if and place the condition inside the component alias--}}
+            <x-badge :show="now()->diffInMinutes($post->created_at) < 30">{{--badge is the alias for
+                component.blade.php , this syntax i found for laravel 7 in the questions on Component aliases tutorial
+                show references var $show in badge.blade.php--}}
+                    Brand new post!
+            </x-badge>{{-- this is the syntax for laravel 7, instead of @badge @endbadge --}}
+            {{--@endif--}}
+        </h1>
+
         <p>{{ $post->content }}</p>
 
         <p>Added {{ $post->created_at->diffForHumans() }}</p>{{--As created_at is by default
@@ -17,13 +28,7 @@
          
 
         
-        @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 30){{-- For more info on Carbon library go to cheat sheet --}}
-            <x-badge>{{--badge is the alias for
-            component.blade.php and 'primary' is another badge class from
-            bootstrap --}}
-                Brand new post!
-            </x-badge>{{-- this is the syntax for laravel 7, instead of @badge @endbadge --}}
-        @endif
+
 
         <h4>Comments</h4>
         {{-- Displaying comments for blogposts --}}
